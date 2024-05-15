9173ec9572af514735e26808b72464bcf29f8007
XXX <muhiddinovhusan0207@gmail.com>
2024-05-15 16:16:59 +0500

    x

--- src/components/interested/index.tsx
+++ src/components/interested/index.tsx
@@ -22,9 +22,135 @@ console.log(products)
                     <ChevronRightIcon />
                 </Link>
 </div>
-     <div className='grid grid-cols-6 max-md:grid-cols-4  max-lg:grid-cols-5 gap-2 '>
+     <div className='grid grid-cols-6 max-md:hidden max-lg:hidden gap-2 '>
 {
     products.slice(6,12).map((item, i)=>(
+        <div key={i} className='w-full rounded-md flex flex-col justify-start items-start gap-3 bg-zinc-100 pb-2 relative transition shadow-lg cursor-pointer'>
+<div className='w-full h-36 md:h-48 relative'>
+
+    <img src={item.images[2]} alt={item.name} className='object-cover rounded-md w-full h-full' />
+</div>
+<div className="relative">
+        <div className="text-sm font-semibold inline-block px-3 py-0.5 bg-red-500 text-white rounded-2xl absolute left-2 -top-8">
+          {Math.round(item.discountPercentage)}%
+        </div>
+        {/* <div>{item.title}</div>
+        <div className="line-through">{item.price}$</div>
+        <div className="">
+          {(
+            item.price -
+            (item.price * item.discountPercentage) / 100
+          ).toFixed(2)}
+          $
+        </div> */}
+                    <div className='flex flex-col gap-2 justify-start items-start text-sm font-medium px-2'>
+                <p
+                    className='line-clamp-2 text-gray-900 text-xs md:text-sm font-normal'
+                >
+                    {item.description}
+                </p>
+                <p className='border rounded-xl text-gray-900 border-yellow-500 p-1 text-xs w-auto bg-amber-200'><span className='font-bold'>{(item.price / 12).toFixed(2)}</span> usd dan/oyiga</p>
+                <p className='line-through text-gray-400'>{item.price} usd</p>
+                <p className='text-red-500'>{(item.price - (item.price * item.discountPercentage) / 100).toFixed(2)} usd</p>
+            </div>
+            <button type='button' className='ml-2 flex items-center gap-1 bg-yellow-500 py-1 px-2 md:py-2 md:px-3 rounded-lg transition hover:bg-yellow-500 hover:bg-opacity-60'>
+                <ShoppingCart className='w-5' />
+                <span className='text-xs md:text-sm'>Savatga</span>
+            </button>
+
+      </div>
+        </div>
+    ))
+}
+
+    </div>
+    <div className='grid grid-cols-3  min-sm:hidden md:hidden gap-2 '>
+{
+    products.slice(6,9).map((item, i)=>(
+        <div key={i} className='w-full rounded-md flex flex-col justify-start items-start gap-3 bg-zinc-100 pb-2 relative transition shadow-lg cursor-pointer'>
+<div className='w-full h-36 md:h-48 relative'>
+
+    <img src={item.images[2]} alt={item.name} className='object-cover rounded-md w-full h-full' />
+</div>
+<div className="relative">
+        <div className="text-sm font-semibold inline-block px-3 py-0.5 bg-red-500 text-white rounded-2xl absolute left-2 -top-8">
+          {Math.round(item.discountPercentage)}%
+        </div>
+        {/* <div>{item.title}</div>
+        <div className="line-through">{item.price}$</div>
+        <div className="">
+          {(
+            item.price -
+            (item.price * item.discountPercentage) / 100
+          ).toFixed(2)}
+          $
+        </div> */}
+                    <div className='flex flex-col gap-2 justify-start items-start text-sm font-medium px-2'>
+                <p
+                    className='line-clamp-2 text-gray-900 text-xs md:text-sm font-normal'
+                >
+                    {item.description}
+                </p>
+                <p className='border rounded-xl text-gray-900 border-yellow-500 p-1 text-xs w-auto bg-amber-200'><span className='font-bold'>{(item.price / 12).toFixed(2)}</span> usd dan/oyiga</p>
+                <p className='line-through text-gray-400'>{item.price} usd</p>
+                <p className='text-red-500'>{(item.price - (item.price * item.discountPercentage) / 100).toFixed(2)} usd</p>
+            </div>
+            <button type='button' className='ml-2 flex items-center gap-1 bg-yellow-500 py-1 px-2 md:py-2 md:px-3 rounded-lg transition hover:bg-yellow-500 hover:bg-opacity-60'>
+                <ShoppingCart className='w-5' />
+                <span className='text-xs md:text-sm'>Savatga</span>
+            </button>
+
+      </div>
+        </div>
+    ))
+}
+
+    </div>
+    <div className='grid grid-cols-4 max-sm:hidden md:hidden gap-2 '>
+{
+    products.slice(6,10).map((item, i)=>(
+        <div key={i} className='w-full rounded-md flex flex-col justify-start items-start gap-3 bg-zinc-100 pb-2 relative transition shadow-lg cursor-pointer'>
+<div className='w-full h-36 md:h-48 relative'>
+
+    <img src={item.images[2]} alt={item.name} className='object-cover rounded-md w-full h-full' />
+</div>
+<div className="relative">
+        <div className="text-sm font-semibold inline-block px-3 py-0.5 bg-red-500 text-white rounded-2xl absolute left-2 -top-8">
+          {Math.round(item.discountPercentage)}%
+        </div>
+        {/* <div>{item.title}</div>
+        <div className="line-through">{item.price}$</div>
+        <div className="">
+          {(
+            item.price -
+            (item.price * item.discountPercentage) / 100
+          ).toFixed(2)}
+          $
+        </div> */}
+                    <div className='flex flex-col gap-2 justify-start items-start text-sm font-medium px-2'>
+                <p
+                    className='line-clamp-2 text-gray-900 text-xs md:text-sm font-normal'
+                >
+                    {item.description}
+                </p>
+                <p className='border rounded-xl text-gray-900 border-yellow-500 p-1 text-xs w-auto bg-amber-200'><span className='font-bold'>{(item.price / 12).toFixed(2)}</span> usd dan/oyiga</p>
+                <p className='line-through text-gray-400'>{item.price} usd</p>
+                <p className='text-red-500'>{(item.price - (item.price * item.discountPercentage) / 100).toFixed(2)} usd</p>
+            </div>
+            <button type='button' className='ml-2 flex items-center gap-1 bg-yellow-500 py-1 px-2 md:py-2 md:px-3 rounded-lg transition hover:bg-yellow-500 hover:bg-opacity-60'>
+                <ShoppingCart className='w-5' />
+                <span className='text-xs md:text-sm'>Savatga</span>
+            </button>
+
+      </div>
+        </div>
+    ))
+}
+
+    </div>
+    <div className='grid grid-cols-5 min-sm:hidden  lg:hidden gap-2 '>
+{
+    products.slice(6,11).map((item, i)=>(
         <div key={i} className='w-full rounded-md flex flex-col justify-start items-start gap-3 bg-zinc-100 pb-2 relative transition shadow-lg cursor-pointer'>
 <div className='w-full h-36 md:h-48 relative'>
 
